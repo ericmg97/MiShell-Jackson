49dab830fa1ca07c7af5134e2a4da69098ab9b32
Eric <ermargar97@gmail.com>
2018-11-02 21:48:05 -0400

    Arreglo general de la estructura del proyecto

--- main.c
+++ main.c
@@ -11,28 +11,17 @@ enum {normal, unknown, cd, cin, cout, ccout, hist};
 
 struct Comand{
     int type;
+    int in_fd;
+    int out_fd;
     bool built_in;
     char** tokens;
     char* text;
-    char* file;
 };
 
 char* history[50];
 int history_count = 0;
 char* path;
 struct Comand comand;
-
-void init_comand(size_t size){
-    comand.text = "";
-    comand.file = "";
-    comand.type = normal;
-    comand.built_in = true;
-    comand.tokens = calloc(20, sizeof(char*));
-    comand.text = malloc(size);
-    comand.file = malloc(strlen(path) + 50);
-    strcpy(comand.file,path);
-    strcat(comand.file,"/");
-}
 
 void try_save_comand() {
     if (!history_count && comand.text[0] != ' ')
@@ -41,43 +30,67 @@ void try_save_comand() {
         history[history_count++] = comand.text;
 }
 
-void parse_comand(char text[], size_t end){
+void compile_comand(char text[], size_t end);
 
-    init_comand(end);
+void init_comand(){
+    comand.in_fd = STDIN_FILENO;
+    comand.out_fd = STDOUT_FILENO;
+    comand.text = "";
+    comand.type = normal;
+    comand.built_in = true;
+    comand.tokens = calloc(20, sizeof(char*));
+    comand.text = malloc(300);
+}
+
+void tokenize(char text[], size_t end){
+
     strncpy(comand.text,text,end - 1);
 
     char* text_clone = malloc(strlen(comand.text));
     strcpy(text_clone,comand.text);
 
-    int subcomands = 0;
     char* token = strtok(text_clone," ");
     comand.tokens[0] = token;
+
     for (int i = 1; i < 20; ++i) {
-        subcomands = i;
+        token = strtok(NULL," ");
+        comand.tokens[i] = token;
         if(token == NULL)
             break;
+    }
+};
+
+void parse_comand(){
+    for (int i = 0; i < 20; ++i) {
+        char* token = comand.tokens[i];
+
+        if(token == NULL)
+            break;
+        else if(strncmp(token,"<",2) == 0){
+            comand.type = cin;
+
+            int file = open(comand.tokens[i + 1],O_RDONLY);
+            comand.in_fd = file;
+        }
         else if(strncmp(token,">>",2) == 0){
             comand.type = ccout;
-            comand.tokens[i] = NULL;
 
-            char* file = strtok(NULL," ");
-            strcat(comand.file,file);
-            break;
+            int file = open(comand.tokens[i + 1],O_APPEND);
+            comand.out_fd = file;
         }
         else if(strncmp(token,">",1) == 0){
             comand.type = cout;
             comand.tokens[i] = NULL;
-
-            char* file = strtok(NULL," ");
-            strcat(comand.file,file);
-            break;
+            //revisar ................................................................
+            int file = open(comand.tokens[i + 1],O_WRONLY,O_TRUNC);
+            comand.out_fd = file;
         }
         else if(strncmp(token,"cd",2) == 0){
             comand.type = cd;
             comand.built_in = false;
 
-            char* folder = strtok(NULL," ");
-            chdir(folder);
+            chdir(comand.tokens[i + 1]);
+            break;
         }
         else if(strncmp(token,"history",7) == 0){
             comand.type = hist;
@@ -88,57 +101,63 @@ void parse_comand(char text[], size_t end){
             char* val = strtok(token,"!");
 
             if(val == NULL){
-                parse_comand(history[history_count - 1], strlen(history[history_count - 1]) + 1);
+                compile_comand(history[history_count - 1], strlen(history[history_count - 1]) + 1);
                 return;
             }
             for (int j = history_count; j > 0; --j) {
                 if(strncmp(history[j - 1],val,strlen(val)) == 0){
-                    parse_comand(history[j - 1], strlen(history[j - 1]) + 1);
+                    compile_comand(history[j - 1], strlen(history[j - 1]) + 1);
                     return;
                 }
             }
             long line = strtol(val,NULL,10);
             if(line > 0 && line <= history_count) {
-                parse_comand(history[line - 1], strlen(history[line - 1]) + 1);
+                compile_comand(history[line - 1], strlen(history[line - 1]) + 1);
                 return;
             }
             comand.type = unknown;
             comand.built_in = false;
             return;
         }
-
-        if(i > 1)
-            comand.tokens[i - 1] = token;
-
-        token = strtok(NULL," ");
     }
     try_save_comand();
 }
 
+void compile_comand(char *text, size_t end) {
+    init_comand();
+    tokenize(text,end);
+    parse_comand();
+}
+
+void print_prompt(){
+    path = getcwd(path,500);
+    write(STDOUT_FILENO,path,strlen(path));
+    write(STDOUT_FILENO," $ ",3);
+}
+
 int main(int argc, char const *argv[]) {
-    while(1) {
-        path = getcwd(path,500);
-        write(STDOUT_FILENO,path,strlen(path));
-        write(STDOUT_FILENO," $ ",3);
+    while (1) {
+        print_prompt();
 
         char text[300];
-        ssize_t end = read(STDIN_FILENO,text,300);
-        parse_comand(text,(size_t)end);
+        ssize_t end = read(STDIN_FILENO, text, 300);
+        compile_comand(text,end);
 
-        int p[2];
-        pipe(p);
+//        int p[2];
+//        pipe(p);
 
         int id = fork();
 
         if (!id) {
-            dup2(p[1], STDOUT_FILENO);
+            dup2(comand.in_fd, STDIN_FILENO);
+            dup2(comand.out_fd, STDOUT_FILENO);
 
-            if(comand.built_in) {
-                if(execvp(comand.tokens[0], (char *const *) comand.tokens) == -1){
+            if (comand.built_in) {
+                if (execvp(comand.tokens[0], (char *const *) comand.tokens) == -1) {
                     printf("Unknown comand \n");
                 }
             }
-            else if(comand.type == hist || comand.type == cout || comand.type == ccout){
+            else if (comand.type == hist) {
                 for (int i = 0; i < 50; ++i) {
                     if (history[i] == NULL)
                         break;
@@ -147,35 +166,23 @@ int main(int argc, char const *argv[]) {
                 }
             }
             exit(0);
-        }
-        else{
+        } else {
             int stat = 0;
-            waitpid(id, &stat, WNOHANG);
+            wait(NULL);
 
-            char buffer[1024];
-            ssize_t stop;
+//            char buffer[1024];
+//            ssize_t stop;
 
-            if(comand.type != unknown && comand.type != cd)
-                stop = read(p[0],buffer,1024);
+//            if (comand.type != unknown && comand.type != cd)
+//                stop = read(comand.out_fd, buffer, 1024);
+//
+//            if (comand.type == normal || comand.type == hist || comand.type == cin)
+//                write(comand.in_fd, buffer, stop);
+        }
 
-            if(comand.type == normal || comand.type == hist){
-                write(STDOUT_FILENO,buffer, stop);
-            }
-            else if(comand.type == cout){
-                FILE* file;
-                file = fopen(comand.file, "w+");
-                fwrite(buffer,stop,sizeof(char),file);
-                fclose(file);
-            }
-            else if(comand.type == ccout){
-                FILE* file;
-                file = fopen(comand.file, "a");
-                fwrite(buffer,stop,sizeof(char),file);
-                fclose(file);
-            }
-            else if(comand.type == unknown){
-                printf("Unkonwn comand \n");
-            }
+        if (comand.type == unknown) {
+            printf("Unkonwn comand \n");
         }
     }
 }
+
